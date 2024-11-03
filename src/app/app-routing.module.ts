3c740968262d2335054f44b8d7916930c52a5add
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-11-03 11:42:45 -0600

    Arreglando la parte visual de la página

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewSalonesComponent } from './salones/view-salones/view-salones.component';
 import { ViewEventosComponent } from './eventos/view-eventos/view-eventos.component';
+import { ViewPersonalComponent } from './personal/view-personal/view-personal.component';
+import { ViewInmueblesComponent } from './inmuebles/view-inmuebles/view-inmuebles.component';
+import { ViewAgendasComponent } from './agendas/view-agendas/view-agendas.component';
 
 const routes: Routes = [
   {path: "salones", component: ViewSalonesComponent},
   {path: "eventos", component: ViewEventosComponent},
+  {path: "personal", component: ViewPersonalComponent},
+  {path: "inmuebles", component: ViewInmueblesComponent},
+  {path: "agenda", component: ViewAgendasComponent}
 ];
 
 @NgModule({
